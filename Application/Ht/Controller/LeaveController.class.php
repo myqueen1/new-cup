e16e73264fe65c9603493707d7401300485a1db1
jw <741265771@qq.com>
2018-08-14 19:29:07 +0800

    no message

--- Application/Ht/Controller/LeaveController.class.php
+++ Application/Ht/Controller/LeaveController.class.php
@@ -16,12 +16,13 @@ class LeaveController extends CommonController
     {
         $leave = M('leave'); // 实例化leave对象
         $count = $leave->where('1=1')->count();// 查询满足要求的总记录数
-        $Page = new \Think\Page($count, 5);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $Page = new \Think\Page($count, 10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show = $Page->show();// 分页显示输出
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
         $list = $leave
             ->join('five_user ON five_user.user_id = five_leave.user_id')
-            ->where('1=1')->order('status')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+            ->order('status')
+            ->where('1=1')->limit($Page->firstRow . ',' . $Page->listRows)->select();
         $this->assign('list', $list);// 赋值数据集
         $this->assign('page', $show);// 赋值分页输出
         $this->display(); // 输出模板
@@ -53,14 +54,14 @@ class LeaveController extends CommonController
     public function reply_do()
     {
         $data = I('post.');
-        $data['status'] = 1;
-        $data['reply_time']=date("Y-m-d H:i:s");
-        $user=cookie('userInfo');
-        $data['admin_id']=$user['admin_id'];
+        $cookie=cookie('user_info');
+        $data['admin_id']=$cookie['id'];
+        $data['status']=1;
+        $data['reply_time']=date('Y-m-d H:i:s');
         $leave = D('leave');
         $res = $leave->updat($data);
         if ($res) {
-            $res = $leave->select_one($data['id']);
+            $res=$leave->find($data['id']);
             echo json_encode($res);
         } else {
             echo false;
