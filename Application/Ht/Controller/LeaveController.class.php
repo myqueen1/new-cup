54639c21d07ce615f21d7d094c8b71be6fffab1e
jw <741265771@qq.com>
2018-08-14 17:20:40 +0800

    留言功能回复

--- Application/Ht/Controller/LeaveController.class.php
+++ Application/Ht/Controller/LeaveController.class.php
@@ -21,10 +21,49 @@ class LeaveController extends CommonController
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
         $list = $leave
             ->join('five_user ON five_user.user_id = five_leave.user_id')
-            ->where('1=1')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+            ->where('1=1')->order('status')->limit($Page->firstRow . ',' . $Page->listRows)->select();
         $this->assign('list', $list);// 赋值数据集
         $this->assign('page', $show);// 赋值分页输出
         $this->display(); // 输出模板
     }
 
+//    删除留言
+    public function delet()
+    {
+        $id = I('get.id');
+        $where = "`id`=" . $id;
+        $res = D('leave')->delet($where);
+        if ($res) {
+            echo true;
+        } else {
+            echo false;
+        }
+    }
+
+//    查看留言信息
+    public function reply_message()
+    {
+        $id = I('get.id');
+        $res = D('leave')->select_one($id);
+        $this->assign('list', $res);
+        $this->display(); // 输出模板
+    }
+
+//    回复消息
+    public function reply_do()
+    {
+        $data = I('post.');
+        $data['status'] = 1;
+        $data['reply_time']=date("Y-m-d H:i:s");
+        $user=cookie('userInfo');
+        $data['admin_id']=$user['admin_id'];
+        $leave = D('leave');
+        $res = $leave->updat($data);
+        if ($res) {
+            $res = $leave->select_one($data['id']);
+            echo json_encode($res);
+        } else {
+            echo false;
+        }
+    }
 }
